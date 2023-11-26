17d1c1aad25aabb89b2a118f09a1b94224400a28
Nour Alkinj <noor22w2@gmail.com>
2023-11-26 22:41:45 +0200

    [Accounts Balances Report]:[under implemenation]

--- app/Http/Controllers/Report/LedgerFilter.php
+++ app/Http/Controllers/Report/LedgerFilter.php
@@ -9,140 +9,183 @@ class LedgerFilter {
 
     public function __construct(public LedgerFilters $filters) {}
 
-    public function sources_filter(array $res = []): array {
-      if(sizeof($this->filters->sources) < 1) return $res;
-      foreach($this->filters->sources as $source) {
-          $res = array_filter($res, function($record) use($source) {
-              return $record['source_name'] == $source["source_name"] &&  $record['source_id'] == $source["source_id"];
-          });
-      }
-      return $res;
-    }
-
-    public function date_range_filter(array $res = []): array {
-      if($this->filters->created_between['from']){
-        $res = array_filter($res, function($record) {
-          dd($record['date'], $this->filters->created_between, $record['date'] == $this->filters->created_between['to']);
-          return $record['date'] > $this->filters->created_between['from'] &&  $record['date'] < $this->filters->created_between['to'];
+    public function sources_filter($res): array {
+        if (empty($this->filters->sources)) return $res;
+           $filteredRecords = array_filter($res, function ($record) {
+            foreach ($this->filters->sources as $source) {
+                if ($record['source_name'] == $source["source_name"] && $record['source_id'] == $source["source_id"]) {
+                    return true;
+                }
+            }
+            return false;
         });
-      }
-      return $res;
+
+        return $filteredRecords;
+    }
+    public function date_range_filter($res): array {
+        $created_between_from = date('Y-m-d', strtotime($this->filters->created_between['from']));
+        $created_between_to = date('Y-m-d', strtotime($this->filters->created_between['to']));
+
+        $res = array_filter($res, function($record) use ($created_between_from,$created_between_to) {
+            return  date('Y-m-d', strtotime($record['date'])) > $created_between_from &&  date('Y-m-d', strtotime($record['date'])) <$created_between_to;
+        });
+        return $res;
     }
 
-    public function account_filter(array $res = []): array {
-      $res = $this->filters->account_id ? JournalEntryRecord::where('account_id', $this->filters->account_id)->get()->toArray() : $res;
-      return $res;
+    public function account_filter($res): array {
+        $res =JournalEntryRecord::where('account_id', $this->filters->account_id)->get()->toArray();
+        return $res;
     }
 
     public function previous_date_filter($res): array {
-      $res = array_filter($res, function($record) {
-          return $record['date'] < $this->filters->created_between['from'];
-      });
-      return $res;
+    $created_between_from = date('Y-m-d', strtotime($this->filters->created_between['from']));
+
+    $res = array_filter($res, function($record) use($created_between_from){
+            return  date('Y-m-d', strtotime($record['date'])) < $created_between_from;
+        });
+        return $res;
     }
 
-    public function after_date_filter(array $res = []): array {
-      $res = array_filter($res, function($record) {
-          return $record['date'] > $this->filters->created_between['to'];
-      });
-      return $res;
+    public function after_date_filter($res): array {
+    $created_between_to = date('Y-m-d', strtotime($this->filters->created_between['to']));
+
+    $res = array_filter($res, function($record)use($created_between_to) {
+            return date('Y-m-d', strtotime($record['date']))  > $created_between_to;
+        });
+        return $res;
     }
 
     public function cost_center_filter($res): array  {
-      $res = array_filter($res, function($record) {
-        return $record['cost_center_id'] == $this->filters->cost_center_id;
-      });
-      return $res;
+        $res =  array_filter($res, function($record) {
+            return $record['cost_center_id'] == $this->filters->cost_center_id;
+        }) ;
+        return $res;
     }
 
     public function branch_filter($res): array  {
-      $res = array_filter($res, function($record) {
-        return $record['branch_id'] == $this->filters->branch_id;
-      });
-      return $res;
+        $res =  array_filter($res, function($record) {
+            return $record['branch_id'] == $this->filters->branch_id;
+        });
+        return $res;
     }
 
-    public function user_filter($res): array  {
-      $res = array_filter($res, function($record) {
-        return $record['user_id'] == $this->filters->user_id;
-      });
-      return $res;
+    public function user_filter($res): array {
+        $res =array_filter($res, function($record) {
+            return $record['user_id'] == $this->filters->user_id;
+        });
+        return $res;
     }
 
     public function client_filter($res): array  {
-      $res = array_filter($res, function($record) {
-        return $record['client_id'] == $this->filters->client_id;
-      });
-      return $res;
+        $res = array_filter($res, function($record) {
+            return $record['client_id'] == $this->filters->client_id;
+        });
+        return $res;
     }
 
     public function contra_account_filter($res): array  {
-      $res = array_filter($res, function($record) {
-        return $record['contra_account_id'] == $this->filters->contra_account_id;
-      });
-      return $res;
+        $res = array_filter($res, function($record) {
+            return $record['contra_account_id'] == $this->filters->contra_account_id;
+        });
+        return $res;
     }
 
-    public function category_filter($res): array  {
-      $res = array_filter($res, function($record) {
-        return $record['category_id'] == $this->filters->category_id;
-      });
-      return $res;
+    public function category_filter($res): array {
+        $res = array_filter($res, function($record) {
+            return $record['category_id'] == $this->filters->category_id;
+        });
+        return $res;
     }
 
     public function employee_filter($res): array {
-      $res = array_filter($res, function($record) {
-        return $record['employee_id'] == $this->filters->employee_id;
-      });
-      return $res;
+        $res = array_filter($res, function($record) {
+            return $record['employee_id'] == $this->filters->employee_id;
+        });
+        return $res;
     }
 
     public function item_filter($res): array {
-      $res = array_filter($res, function($record) {
-        return $record['item_id'] == $this->filters->item_id;
-      });
-      return $res;
+        $res = array_filter($res, function($record) {
+            return $record['item_id'] == $this->filters->item_id;
+        });
+        return $res;
     }
 
     public function asset_filter($res): array {
-      $res = array_filter($res, function($record) {
-        return $record['asset_id'] ==  $this->filters->asset_id;  
-      });
-      return $res;
-    }
-    
-    public function not_contains_filter($res) {
-      $res = array_filter($res, function($record) {
-        return !str_contains($record['notes'], $this->filters->contains);  
-      });
-      return $res;
-    }
-    
-    public function contains_filter($res) {
-      $res = array_filter($res, function($record) {
-        return str_contains($record['notes'], $this->filters->contains);  
-      });
-      return $res;
+        $res = array_filter($res, function($record) {
+            return $record['asset_id'] == $this->filters->asset_id;
+        });
+        return $res;
     }
 
-    public function posted_date_range_filter($res): array {
-      if($this->filters->posted_between['from']) {
+    public function contains_filter($res): array  {
         $res = array_filter($res, function($record) {
-            return $record['post_to_account_date'] > $this->filters->posted_between['from'] &&  $record['post_to_account_date'] < $this->filters->posted_between['to'];
+            return str_contains($record['notes'], $this->filters->contains);
         });
-      }
-      return $res;
+        return $res;
     }
 
-    public function not_posted_date_range_filter($res): array {
-      if($this->filters->posted_between['from']) {
+    public function not_contains_filter($res): array  {
         $res = array_filter($res, function($record) {
-            if(!$record['is_post_to_account']) {
-              return $record['date'] > $this->filters->posted_between['from'] &&  $record['date'] < $this->filters->posted_between['to'];
-            }
+            return !str_contains($record['notes'],$this->filters->not_contains);
         });
-      }
-      return $res;
+        return $res;
     }
+    public function posted_date_range_filter($res): array  {
+    $posted_between_from = date('Y-m-d', strtotime($this->filters->posted_between['from']));
+    $posted_between_to = date('Y-m-d', strtotime($this->filters->posted_between['to']));
+
+    $res = array_filter($res, function($record) use($posted_between_from,$posted_between_to){
+            return date('Y-m-d', strtotime($record['post_to_account_date'])) > $posted_between_from &&  date('Y-m-d', strtotime($record['post_to_account_date'])) < $posted_between_to;
+        });
+        return $res;
+    }
+
+    public function not_posted_date_range_filter($res): array  {
+    $posted_between_from = date('Y-m-d', strtotime($this->filters->posted_between['from']));
+    $posted_between_to = date('Y-m-d', strtotime($this->filters->posted_between['to']));
+
+    $res = array_filter($res, function($record)use($posted_between_from,$posted_between_to) {
+        if(!$record['is_post_to_account']) {
+            return date('Y-m-d', strtotime($record['date'])) > $posted_between_from && date('Y-m-d', strtotime($record['date'])) < $posted_between_to ;
+        }
+    });
+    return $res;
+    }
+
+    public function debit_only_filter($res): array  {
+        $res = array_filter($res, function($record) {
+            return $record['debit'] > 0;
+        });
+        return $res;
+    }
+    public function credit_only_filter($res): array  {
+        $res = array_filter($res, function($record) {
+            return $record['credit'] > 0;
+        });
+        return $res;
+    }
+    public function all_filter($res): array  {
+        $res = array_filter($res, function($record) {
+            return ($record['credit'] > 0 || $record['debit'] > 0);
+        });
+        return $res;
+    }
+    public function is_post_to_account_filter($res): array  {
+        $res = array_filter($res, function($record) {
+            return $record['is_post_to_account'];
+        });
+        return $res;
+    }
+    public function is_not_post_to_account_filter($res): array  {
+        $res = array_filter($res, function($record) {
+            return !($record['is_post_to_account']);
+        });
+        return $res;
+    }
+
+
+
+
 
 }
---
+++ vendor/sajya/server/src/Http/Parser.php
@@ -0,0 +1,165 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sajya\Server\Http;
+
+use Exception;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Validator;
+use Sajya\Server\Exceptions\InvalidParams;
+use Sajya\Server\Exceptions\InvalidRequestException;
+use Sajya\Server\Exceptions\ParseErrorException;
+use Sajya\Server\Rules\Identifier;
+use TypeError;
+
+class Parser
+{
+    /**
+     * Flag indicating a parse error.
+     */
+    protected bool $isParseError = false;
+
+    /**
+     * Raw content.
+     */
+    protected string $content;
+
+    /**
+     * Extract content.
+     */
+    protected Collection $decode;
+
+    /**
+     * @var bool
+     */
+    protected bool $batching = false;
+
+    /**
+     * @var bool
+     */
+    protected bool $notification = false;
+
+    /**
+     * ContentValidation constructor.
+     *
+     * @param string $content
+     */
+    public function __construct(string $content = '')
+    {
+        $this->content = $content;
+
+        try {
+            $decode = json_decode($content, true, 512, JSON_THROW_ON_ERROR);
+
+            $this->decode = collect($decode);
+            $this->batching = $this->decode->isNotEmpty() && Arr::isList($this->decode->toArray());
+
+            $emptyIdRequest = $this->decode
+                ->when(! $this->batching, fn ($request) => collect([$request]))
+                ->first(fn ($value) => ! isset($value['id']));
+
+            $this->notification = $emptyIdRequest !== null;
+        } catch (Exception | TypeError $e) {
+            $this->decode = collect();
+            $this->isParseError = true;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isError(): bool
+    {
+        return $this->isParseError;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getContent(): Collection
+    {
+        return $this->decode;
+    }
+
+    /**
+     * @return Request[]|Exception[]
+     */
+    public function makeRequests(): array
+    {
+        $content = $this->getContent();
+
+        if ($this->isBatch()) {
+            return $content
+                ->map(fn ($options) => $this->checkValidation($options))
+                ->whenEmpty(fn (Collection $collection) => $collection->push($this->checkValidation()))
+                ->map(fn ($options) => $options instanceof Exception ? $options : Request::loadArray($options))
+                ->toArray();
+        }
+
+        $options = $this->checkValidation($content->toArray());
+
+        return [is_array($options) ? Request::loadArray($options) : $options];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBatch(): bool
+    {
+        return $this->batching;
+    }
+
+    /**
+     * Does the request contain at least one notification message
+     *
+     * @return bool
+     */
+    public function isNotification(): bool
+    {
+        return $this->notification;
+    }
+
+    /**
+     * @param bool|string|array|int $options
+     *
+     * @return InvalidParams|ParseErrorException|InvalidRequestException|array
+     */
+    public function checkValidation($options = [])
+    {
+        if ($this->isError()) {
+            return new ParseErrorException();
+        }
+
+        if (! is_array($options) || Arr::isList($options)) {
+            return new InvalidRequestException();
+        }
+
+        $data = $options;
+
+        // skip deep parameters for validator
+        if (isset($options['params']) && is_array($options['params'])) {
+            $data['params'] = [];
+        }
+
+        $validation = Validator::make($data, self::rules());
+
+        return $validation->fails()
+            ? new InvalidParams($validation->errors()->toArray())
+            : $options;
+    }
+
+    /**
+     * @return array
+     */
+    public static function rules(): array
+    {
+        return [
+            'jsonrpc' => 'required|in:"2.0"',
+            'method'  => 'required|string',
+            'params'  => 'array',
+            'id'      => new Identifier(),
+        ];
+    }
+}
