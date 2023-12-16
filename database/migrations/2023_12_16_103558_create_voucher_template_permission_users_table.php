629b86f97fe2f0938b829f1ae023bf16ac9f1cbd
Clauda <klodaalrakkad@gmail.com>
2023-12-16 13:32:27 +0200

    Voucher Permission & Bill Permission

---
+++ database/migrations/2023_12_16_103558_create_voucher_template_permission_users_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up()
+    {
+        Schema::create('voucher_template_permission_users', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('voucher_permission_id');
+            $table->unsignedBigInteger('voucher_template_id');
+            $table->timestamps();
+
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('voucher_template_permission_users');
+    }
+};
